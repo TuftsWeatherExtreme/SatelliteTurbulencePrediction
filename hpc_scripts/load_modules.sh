f09c11cc2ccd9defba9af5603092949460ef6ca3
eelkus01 <eelkus01@login-p01.pax.tufts.edu>
2026-03-31 10:45:23 -0400

    Added srun to load_modules

--- hpc_scripts/load_modules.sh
+++ hpc_scripts/load_modules.sh
@@ -14,13 +14,16 @@ fi
 echo "Resetting environment"
 source "$(dirname ${BASH_SOURCE[0]})/unload_modules.sh"
 
+echo "Allocating resources with slurm..."
+srun -p gpu --gres=gpu:1 -n 4 --mem=8g -t 1-0 --pty bash
+
 echo "Loading cuda and miniforge..."
 module load cuda/12.9 miniforge/25.3.0
 
 echo "Activating conda environment..."
-conda activate $ENV_PATH
+conda activate $SAT_ENV_PATH
 
 echo "Redirecting into the satellite repo..."
-cd $REPO_PATH
+cd $SAT_REPO_PATH
 
 echo "Done! Environment has now been set up"
